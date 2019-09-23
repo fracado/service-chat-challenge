843497e5ab2748f771396de999fce26dc20c682a
fbath.subtel <f.bath@subtel.de>
2019-09-23 22:29:36 +0200

    added input box + added reactstrap

--- src/components/chat/InputContainer.js
+++ src/components/chat/InputContainer.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Container, Form, Input } from 'reactstrap';
 
 export default class InputContainer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      text: ""
+    }
+  }
+
+  handleSubmit(e){
+    e.preventDefault()
+    this.props.handleNewMessage(this.state.text)
+    this.setState({ text: '' })
+  }
+  
   render() {
     return (
-      <div className="inputbox">
-        
-      </div>
+      <Container className="inputbox">
+        <Form onSubmit={e => this.handleSubmit(e)}>
+            <Input
+              className="inputfield"
+              type="text"
+              placeholder={'Enter message...'}
+              value={this.state.text}
+              onChange={e => this.setState({ text: e.target.value })}></Input>
+        </Form>
+      </Container>
     )
   }
 }
