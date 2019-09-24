9ea90899bdd91824d480de0643d741326f14df21
fbath.subtel <f.bath@subtel.de>
2019-09-24 10:21:27 +0200

    added usermessage function & botresponse logic

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,11 +7,44 @@ export default class App extends Component {
   constructor(props) {
     super(props)
 
+    this.handleNewMessage = this.handleNewMessage.bind(this);
+
     this.state = {
       messages: [
         { user: false, 
           text: "Hello and welcome to our Service Chat! Please give us your email address to see your order status!"},
-      ]
+      ],
+      orders: null
+    }
+  }
+
+  handleNewMessage(userMessage) {
+    let newMessages = []
+    let newUserMessage = {
+      user: true,
+      text: userMessage
+    }
+    newMessages.push(newUserMessage);
+
+    if (userMessage.includes('@')) {
+      fetch('https://demo7609961.mockable.io/orders/?CUSTOMER_EMAIL' + userMessage)
+      .then(res => res.json())
+      .then((result) => {
+        let orderResult = result;
+        let newOrderMessage = {
+          user: false,
+          order: true
+        }
+        newMessages.push(newOrderMessage);
+        this.setState({ messages: [...this.state.messages, ...newMessages], orders: orderResult.orders})
+      })
+    } else {
+        let newBotMessage = {
+          user: false,
+          text: "Please enter your email address to check the status of your orders."
+        }
+        newMessages.push(newBotMessage);
+        this.setState({ messages: [...this.state.messages, ...newMessages]})
     }
   }
 
@@ -20,8 +53,8 @@ export default class App extends Component {
     return (
       <div className="App">
       <header className="App-header">
-        <ChatContainer messages={this.state.messages}/>
-        <InputContainer />
+        <ChatContainer messages={this.state.messages} orders={this.state.orders}/>
+        <InputContainer handleNewMessage={this.handleNewMessage}/>
       </header>
     </div>
     )
--- src/components/chat/ChatContainer.js
+++ src/components/chat/ChatContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardBody, CardText } from 'reactstrap';
+import OrderMessage from './OrderMessage';
 
 export default class ChatContainer extends Component {
   constructor(props) {
@@ -12,7 +13,7 @@ export default class ChatContainer extends Component {
         <Card>
           <CardBody>
             <CardText className="message">{this.props.messages.map(
-              message => <div className={message.user ? 'align-right' : 'align-left'}>{message.text}</div>
+              message => <div className={message.user ? 'align-right' : 'align-left'}>{message.order ? <OrderMessage orders={this.props.orders}/> : message.text}</div>
             )}
             </CardText>
           </CardBody>
