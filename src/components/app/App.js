06f99e56f1a4488ed96482d919ba994f0ade9a49
fbath.subtel <f.bath@subtel.de>
2019-09-24 07:57:39 +0200

    added message(s)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,11 +4,23 @@ import ChatContainer from '../chat/ChatContainer';
 import InputContainer from '../chat/InputContainer';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      messages: [
+        { user: false, 
+          text: "Hello and welcome to our Service Chat! Please give us your email address to see your order status!"},
+      ]
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
       <header className="App-header">
-        <ChatContainer />
+        <ChatContainer messages={this.state.messages}/>
         <InputContainer />
       </header>
     </div>
--- src/components/chat/ChatContainer.js
+++ src/components/chat/ChatContainer.js
@@ -1,10 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Card, CardBody, CardText } from 'reactstrap';
 
 export default class ChatContainer extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
       <div className="chatbox">
-        
+        <Card>
+          <CardBody>
+            <CardText className="message">{this.props.messages.map(
+              message => <div className={message.user ? 'align-right' : 'align-left'}>{message.text}</div>
+            )}
+            </CardText>
+          </CardBody>
+        </Card>
       </div>
     )
   }
